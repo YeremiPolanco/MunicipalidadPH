483b54beb67bb57f783be3be7cd8d23eb326df6b
YeremiPolanco <yeremi.polanco.julca@gmail.com>
2024-07-03 14:14:56 -0500

    first commit

---
+++ src/main/java/ms/jiren/municipalidad/MunaPhBackendApplication.java
@@ -0,0 +1,13 @@
+package ms.jiren.municipalidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunaPhBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MunaPhBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ms/jiren/municipalidad/MunaPhBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.jiren.municipalidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunaPhBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
